f0459a1d7ac3ad731020ef145ae76dcef865ac4d
Jibola Paul <harjibbolar26@gmail.com>
2024-10-10 12:50:11 +0100

    fix type errors

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Navbar from "./navbar";
 import SideNav from "./sidenav";
 import BookingForm from "./bookingform";
